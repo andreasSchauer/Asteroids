3cd5a5f5293c50f7141b5ce62b5de07e845a28be
andreasSchauer <andreas.schauer@yahoo.com>
2024-08-28 13:51:12 +0200

    E: added movement to Player

--- main.py
+++ main.py
@@ -15,11 +15,13 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
-        screen.fill((0,0,0))
-        player.draw(screen)
 
+        player.update(dt)
+
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        
         dt = clock.tick(60) / 1000
 
 
